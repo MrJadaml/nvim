c95c3c7a4bf81290029504a49d383e8236dc7c4f
Foo <spadam77@gmail.com>
2023-10-06 10:20:43 -0700

    added cheetsheet

--- lua/jadam/remap.lua
+++ lua/jadam/remap.lua
@@ -1,21 +1,21 @@
 -- Open NetRW
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>rw', vim.cmd.Ex)
 
 -- Move highlighted text up and down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 -- Remove search highlight
-vim.keymap.set("n", "<Esc><Esc>", "<Esc>:nohlsearch<CR><Esc>", { noremap = true, silent = true })
+vim.keymap.set('n', '<Esc><Esc>', '<Esc>:nohlsearch<CR><Esc>', { noremap = true, silent = true })
 
 -- Copy
 vim.cmd('set clipboard+=unnamedplus')
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set('n', '<leader>y', '\"+y')
+vim.keymap.set('v', '<leader>y', '\"+y')
+vim.keymap.set('n', '<leader>Y', '\"+Y')
 
 -- Preserve copied text when pasting
-vim.keymap.set("x", "<leader>p", "\"_dp")
+vim.keymap.set('x', '<leader>p', '\"_dp')
 
 -- Highlight flash on yank
 -- See `:help vim.highlight.on_yank()`
@@ -29,5 +29,5 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Disable Ex mode
-vim.keymap.set("n", "Q", "<Nop>", { noremap = true })
+vim.keymap.set('n', 'Q', '<Nop>', { noremap = true })
 
