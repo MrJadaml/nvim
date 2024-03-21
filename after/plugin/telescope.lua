cf0ac171c7fc78096e94ec0217c9e615b864316d
mrjadaml <spadam77@gmail.com>
2024-03-20 20:35:06 -0700

    typescript, indent, copilot, prettier

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,7 +2,23 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<C-g>', builtin.git_files, {})
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+
 vim.keymap.set('n', '<leader>ps', function()
-	-- This appears to not be working --
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+-- setup hidden_files defaults
+vim.g.telescope_defaults = {
+  hidden_files = true,
+}
+
+require('telescope').setup {
+  defaults = {
+    file_ignore_patterns = {
+      ".git/*",
+      "node_modules/*",
+      "%.lock",
+    },
+    hidden_files = false
+  }
+}
